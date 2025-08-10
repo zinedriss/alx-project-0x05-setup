4cf988236960fc5cf2a1ae1d0d1435ef8fa0bb24
idriss <ssenuoy.issiurd@gmail.com>
2025-08-10 17:33:35 +0100

    khk

---
+++ alx-project-0x07/alx-project-0x08/components/layouts/Layout.tsx
@@ -0,0 +1,18 @@
+// components/layouts/Layout.tsx
+import { ReactComponentProps } from "@/interfaces";
+import Footer from "./Footer"
+import Header from "./Header"
+
+const Layout: React.FC<ReactComponentProps> = ({ children }) => {
+  return (
+    <>
+      <Header />
+      <main className="min-h-screen">
+        {children}
+      </main>
+      <Footer />
+    </>
+  )
+}
+
+export default Layout;
